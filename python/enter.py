49dab25a6316512161bbec5320719b59483d4d8a
callasio <callasio39@gmail.com>
2023-10-10 17:42:53 +0900

    Update

--- python/enter.py
+++ python/enter.py
@@ -2,19 +2,27 @@ import sys
 import subprocess
 import requests
 from os import path
+import threading
 
-if __name__ == '__main__':
+
+def start_server():
     source_path = path.realpath(__file__)
     source_dir = path.dirname(source_path)
 
     interpreter = sys.executable
     contest_number = sys.argv[1]
-    print("Entering contest:", contest_number)
 
-    process = subprocess.Popen([
+    _ = subprocess.Popen([
         interpreter,
         path.join(source_dir, 'web/main.py')
     ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    new_http = requests.request("GET", f"http://127.0.0.1:5000/e/{contest_number}")
+    new_http = requests.get(f"http://127.0.0.1:5000/e/{contest_number}")
     print(new_http.text)
+
+
+if __name__ == '__main__':
+    print("Entering contest:", sys.argv[1])
+    server_thread = threading.Thread(target=start_server)
+    server_thread.start()
+    server_thread.join()
--- python/quit.py
+++ python/quit.py
@@ -1,2 +1,7 @@
+import requests
+
 if __name__ == '__main__':
-    print("Quit contest")
+    try:
+        requests.get(f"http://127.0.0.1:5000/q")
+    except requests.exceptions.ConnectionError:
+        print("Quit successfully")
